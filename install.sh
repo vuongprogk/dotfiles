6e8f3efcdb361207772776903825e8d5d488a08c
vuongprogk <vuong753333@gmail.com>
2025-10-23 02:02:04 +0700

    initial dotfile

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+curl -sSL https://dot.net/v1/dotnet-install.sh | bash /dev/stdin --channel 8.0 --install-dir /usr/share/dotnet \
+  && ln -s /usr/share/dotnet/dotnet /usr/bin/dotnet
+echo "DOTNET_ROOT=/usr/share/dotnet" >> /etc/environment
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl" \
+  && install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl \
+  && rm kubectl
